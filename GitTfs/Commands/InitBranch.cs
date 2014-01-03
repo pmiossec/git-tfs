313941dae5f189a529f24bc031650058612f7deb
Philippe Miossec <pmiossec@gmail.com>
2014-01-03 18:43:29 +0100

    Get the TfsRemotes if branch was already initialized

--- GitTfs/Commands/InitBranch.cs
+++ GitTfs/Commands/InitBranch.cs
@@ -208,6 +208,7 @@ namespace Sep.Git.Tfs.Commands
                     var branchDatas = new BranchDatas
                         {
                             TfsRepositoryPath = tfsBranchPath.TfsRepositoryPath,
+                            TfsRemote = _globals.Repository.ReadAllTfsRemotes().FirstOrDefault(r => r.TfsRepositoryPath == tfsBranchPath.TfsRepositoryPath)
                         };
                     try
                     {
