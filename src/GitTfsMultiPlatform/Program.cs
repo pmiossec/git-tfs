2b76c40eeb0b3b9cfab831f7b25cfdce316a68c3
Philippe Miossec <pmiossec@gmail.com>
2018-12-07 14:17:29 +0100

    Create a GitTfs project to target multi plateform

---
+++ src/GitTfsMultiPlatform/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GitTfs
+{
+    //The goal of this project is only to be able to run Git-tfs on other platforms than windows
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Environment.ExitCode = ProgramHelper.MainCore(args);
+            }
+            catch (Exception e)
+            {
+                ProgramHelper.ReportException(e);
+                Environment.ExitCode = GitTfsExitCodes.ExceptionThrown;
+            }
+        }
+    }
+}
